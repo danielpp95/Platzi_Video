6db0a5124b6883a621c949d6afba8a2d25d9a870
Daniel Pereira <danielpp95@mail.com>
2019-08-25 22:21:01 +0100

    added not found page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Containers
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Register from './containers/Register';
+import NotFound from './containers/NotFound';
 
+// Router
 const App = () => (
     <BrowserRouter>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/Login' component={Login} />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route component={NotFound} />
+        </Switch>
     </BrowserRouter>
 );
 
+// DOM
 ReactDom.render(<App />, document.getElementById('app'));
