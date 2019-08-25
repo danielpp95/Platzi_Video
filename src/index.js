77d0b3ba5158f7908759ed066a1c0f94218db4c2
Daniel Pereira <danielpp95@mail.com>
2019-08-25 22:01:39 +0100

    added router and login page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import Login from './containers/Login';
 
-import App from './containers/App'
+const App = () => (
+    <BrowserRouter>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Login' component={Login} />
+    </BrowserRouter>
+);
 
-ReactDom.render(<App/>, document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,9 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        historyApiFallback: true,
+    },
     plugins: [
         new HtmlWebpackPLugin({
             template: './public/index.html',
